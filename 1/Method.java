0bbf5e88be5dc2667da94dcf7de633d42b22ef3d
Kuzafri <64196366+kuzafri@users.noreply.github.com>
2022-01-22 18:20:03 +0000

    if else

---
+++ 1/Method.java
@@ -0,0 +1,32 @@
+
+public class Method {
+	
+	public <T> void reverse(T[] data){
+		int low =0;
+		int high = data.length -1;
+		
+		while(low < high){
+			//swapping the position
+			T temp = data[low];
+			data[low++] = data[high];
+			data[high--] = temp;
+		}
+	}
+	
+
+	
+	public static void main(String[] args){
+		String[] stringArr = {"a", "b", "c", "d"};
+		Integer[] intArr = {1, 2, 3, 4};
+		
+		Method gmd = new Method();
+		gmd.reverse(stringArr);
+		for(int i=0; i< stringArr.length; i++)
+			System.out.println(stringArr[i]);
+		
+		gmd.reverse(intArr);
+		for(int i=0; i< intArr.length; i++)
+			System.out.println(intArr[i]);
+	}
+
+}
