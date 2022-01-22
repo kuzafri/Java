84d2cde9879f5821f53b08645d93dd3016120943
Kuzafri <64196366+kuzafri@users.noreply.github.com>
2022-01-22 14:21:13 +0000

    notes

--- 1/Basic4.java
+++ 1/Basic4.java
@@ -8,9 +8,10 @@ public class Basic4 {
 
 	public static void main(String[] args){
 
+    // Untuk stack ni yang mana dia push kemudian tu yang tu keluar dulu bila pop
 		Stack s = new Stack();
 		s.push("cat");
-		s.push("dog");
+		s.push("meow");
 		String thing = (String) s.pop();
 		
 		System.out.println(thing);
